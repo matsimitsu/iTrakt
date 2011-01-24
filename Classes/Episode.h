10cd14db6eff3bf9317e93a47bd8bb7af71bde22
Eloy Duran <eloy.de.enige@gmail.com>
2011-01-24 20:36:37 +0100

    Download and cache JSON for a specific episode.

--- Classes/Episode.h
+++ Classes/Episode.h
@@ -4,9 +4,18 @@
 @interface Episode : NSObject {
   id delegate;
   BroadcastDate *broadcastDate;
+
+  NSData *episodeData;
+  NSDictionary *episodeDict;
+
   UIImage *banner;
   NSString *bannerURL;
   NSData *bannerData;
+
+  UIImage *poster;
+  NSData *posterData;
+
+  NSString *tvdbID;
   NSString *title;
   NSUInteger season;
   NSUInteger number;
@@ -14,9 +23,15 @@
 
 @property (nonatomic, assign) id delegate;
 @property (nonatomic, assign) BroadcastDate *broadcastDate;
+
 @property (nonatomic, retain) UIImage *banner;
 @property (nonatomic, retain) NSString *bannerURL;
 @property (nonatomic, retain) NSData *bannerData;
+
+@property (nonatomic, retain) UIImage *poster;
+@property (nonatomic, retain) NSData *posterData;
+
+@property (nonatomic, retain) NSString *tvdbID;
 @property (nonatomic, retain) NSString *title;
 @property (nonatomic, assign) NSUInteger season;
 @property (nonatomic, assign) NSUInteger number;
--- Classes/Episode.m
+++ Classes/Episode.m
@@ -9,6 +9,11 @@
 @synthesize banner;
 @synthesize bannerURL;
 @synthesize bannerData;
+
+@synthesize poster;
+@synthesize posterData;
+
+@synthesize tvdbID;
 @synthesize title;
 @synthesize season;
 @synthesize number;
@@ -24,18 +29,42 @@
 
     self.bannerURL = [dict valueForKeyPath:@"show.banner"];
 
-    if ([[EGOCache currentCache] hasCacheForKey:[self.bannerURL lastPathComponent]]) {
-      //NSLog(@"Banner was cached!");
-      self.banner = [UIImage imageWithData:[[EGOCache currentCache] dataForKey:[self.bannerURL lastPathComponent]]];
+    NSString *apiKey = @"6ce9688a2fd3316355204b1d25d44f00";
+    self.tvdbID = [dict valueForKeyPath:@"show.tvdb_id"];
+
+    //self.posterData = [NSMutableData data];
+
+    if ([[EGOCache currentCache] hasCacheForKey:tvdbID]) {
+      NSLog(@"Load episode data from cache for tvdb ID `%@'", tvdbID);
+      episodeData = [[EGOCache currentCache] dataForKey:tvdbID];
+      episodeDict = [[episodeData yajl_JSON] retain];
+      [episodeData release];
     } else {
-      //NSLog(@"Start image download: %@", self.bannerURL);
-      self.bannerData = [NSMutableData data];
-      NSURL *url = [NSURL URLWithString:self.bannerURL];
+      episodeData = [[NSMutableData data] retain];
+      NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://api.trakt.tv/show/summary.json/%@/%@", apiKey, tvdbID, nil]];
+      NSLog(@"Download episode data from for tvdb ID `%@'", tvdbID);
       NSURLRequest *request = [NSURLRequest requestWithURL:url];
       [NSURLConnection connectionWithRequest:request delegate:self];
     }
+
+    //NSLog(@"Start image download: %@", self.bannerURL);
+    //if ([[EGOCache currentCache] hasCacheForKey:[self.bannerURL lastPathComponent]]) {
+      ////NSLog(@"Banner was cached!");
+      //self.banner = [UIImage imageWithData:[[EGOCache currentCache] dataForKey:[self.bannerURL lastPathComponent]]];
+    //} else {
+      ////NSLog(@"Start image download: %@", self.bannerURL);
+      //self.bannerData = [NSMutableData data];
+      //NSURL *url = [NSURL URLWithString:self.bannerURL];
+      //NSURLRequest *request = [NSURLRequest requestWithURL:url];
+      //[NSURLConnection connectionWithRequest:request delegate:self];
+    //}
   }
   return self;
+}
+
+- (void)dealloc {
+  [super dealloc];
+  [episodeDict release];
 }
 
 - (NSString *)numberText {
@@ -43,20 +72,39 @@
 }
 
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-  [bannerData appendData:data];
+  [episodeData appendData:data];
 }
 
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection {
-  //NSLog(@"Connection finished!");
-  self.banner = [UIImage imageWithData:bannerData];
-  [bannerData release];
-  [[EGOCache currentCache] setImage:self.banner forKey:[self.bannerURL lastPathComponent]];
-  [self.delegate performSelector:@selector(episodeDidLoadBanner:) withObject:self];
+  NSLog(@"Episode data download finished!");
+  episodeDict = [[episodeData yajl_JSON] retain];
+  [[EGOCache currentCache] setData:episodeData forKey:tvdbID];
+  [episodeData release];
+  //episodeDict = [[arrayFromData objectAtIndex:0] retain];
+  //NSLog(@"%@", episodeDict);
+  //[[EGOCache currentCache] setPlist:episodeDict forKey:[NSString stringWithFormat:@"%@.plist", tvdbID, nil]];
 }
 
 - (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
-  NSLog(@"Image download failed: %@", [error localizedDescription]);
-  [bannerData release];
+  NSLog(@"Episode data download failed: %@", [error localizedDescription]);
+  [episodeData release];
 }
+
+//- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+  //[bannerData appendData:data];
+//}
+
+//- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+  ////NSLog(@"Connection finished!");
+  //self.banner = [UIImage imageWithData:bannerData];
+  //[bannerData release];
+  //[[EGOCache currentCache] setImage:self.banner forKey:[self.bannerURL lastPathComponent]];
+  //[self.delegate performSelector:@selector(episodeDidLoadBanner:) withObject:self];
+//}
+
+//- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+  //NSLog(@"Image download failed: %@", [error localizedDescription]);
+  //[bannerData release];
+//}
 
 @end

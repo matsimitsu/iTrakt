bcd4d797e78dd83bb586d825df657f21966f7cd5
Eloy Duran <eloy.de.enige@gmail.com>
2011-02-26 13:38:50 +0100

    Import Base64 source from Cyrus Najmabadi: http://www.cocoadev.com/index.pl?BaseSixtyFour

---
+++ Classes/Base64.h
@@ -0,0 +1,15 @@
+// From Cyrus Najmabadi: http://www.cocoadev.com/index.pl?BaseSixtyFour
+// Available in the public domain.
+
+#import <Foundation/Foundation.h>
+
+
+@interface Base64 : NSObject
+
++ (NSString *)encode:(const uint8_t*)input length:(NSInteger)length;
++ (NSString *)encode:(NSData *)rawBytes;
+
++ (NSData *)decode:(const char *)string length:(NSInteger)inputLength;
++ (NSData *)decode:(NSString *)string;
+
+@end

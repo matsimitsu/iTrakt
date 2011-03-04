16d0fd719987579efb140e0255bbec8d16035ea2
Eloy Duran <eloy.de.enige@gmail.com>
2011-03-04 20:36:37 +0100

    Draw checkmark. Still need to a small curve to it.

--- Classes/Views/Checkbox.m
+++ Classes/Views/Checkbox.m
@@ -33,10 +33,35 @@ addRoundedRect(CGContextRef ctx, CGRect rect, float cornerRadius) {
 @implementation CheckboxDrawing
 
 - (void)drawInContext:(CGContextRef)context {
-  CGContextSetRGBStrokeColor(context, 0.53, 0.53, 0.53, 1);
-  CGContextSetLineWidth(context, 2);
-  addRoundedRect(context, CGRectInset(self.bounds, 0.5, 0.5), 6);
+  //CGContextSetRGBStrokeColor(context, 0.53, 0.53, 0.53, 1);
+  CGContextSetRGBStrokeColor(context, 0.25, 0.25, 0.25, 1);
+
+  CGContextSetLineWidth(context, 1.5);
+  addRoundedRect(context, CGRectInset(self.bounds, 2, 2), 3);
   CGContextStrokePath(context);
+
+  CGContextSaveGState(context);
+  CGContextSetBlendMode(context, kCGBlendModeClear);
+  CGContextMoveToPoint(   context, 16, 2);
+  CGContextAddLineToPoint(context, 20, 1);
+  CGContextAddLineToPoint(context, 20, 4);
+  CGContextAddLineToPoint(context, 17, 7);
+  CGContextAddLineToPoint(context, 16, 2);
+  CGContextFillPath(context);
+
+  CGContextRestoreGState(context);
+  CGContextSetRGBFillColor(context, 0.25, 0.25, 0.25, 1);
+  CGContextSetLineWidth(context, 1);
+  CGContextMoveToPoint(context, 7, 8);
+  CGContextAddLineToPoint(context, 10, 11);
+  CGContextAddLineToPoint(context, 19, 1);
+  CGContextAddLineToPoint(context, 20, 2);
+  CGContextAddLineToPoint(context, 10, 16);
+  //CGContextAddLineToPoint(context, 10, 14);
+  CGContextAddLineToPoint(context, 5.5, 9.5);
+  CGContextAddLineToPoint(context, 7, 8);
+
+  CGContextFillPath(context);
 }
 
 @end
@@ -45,6 +70,8 @@ addRoundedRect(CGContextRef ctx, CGRect rect, float cornerRadius) {
 
 - (id)initWithFrame:(CGRect)frame {
   if (self = [super initWithFrame:frame]) {
+    NSLog(@"Complete frame %@", NSStringFromCGRect(frame));
+    
     CALayer *layer = self.layer;
     
     CAGradientLayer *boxBackground = [CAGradientLayer layer];
@@ -52,15 +79,17 @@ addRoundedRect(CGContextRef ctx, CGRect rect, float cornerRadius) {
     UIColor *endColor    = [UIColor colorWithWhite:0.83 alpha:1.0];
     boxBackground.colors = [NSArray arrayWithObjects:(id)startColor.CGColor, (id)endColor.CGColor, nil];
     //boxBackground.locations = [NSArray arrayWithObjects:[NSNumber numberWithFloat:0.0], [NSNumber numberWithFloat:1.0], nil];
-    boxBackground.cornerRadius = 4.0;
+    boxBackground.cornerRadius = 3.0;
     //boxBackground.frame = CGRectMake(2, 2, frame.size.width - 2, frame.size.height - 2);
-    boxBackground.frame = CGRectMake(2, 2, 26, 26);
-    NSLog(@"BG frame: %@", NSStringFromCGRect(boxBackground.frame));
+    boxBackground.frame = CGRectMake(2, 2, 17, 17);
+    boxBackground.edgeAntialiasingMask = 0;
+    NSLog(@"BG edge antialias: %d BG frame: %@", boxBackground.edgeAntialiasingMask, NSStringFromCGRect(boxBackground.frame));
     [layer addSublayer:boxBackground];
 
     CALayer *drawing = [CheckboxDrawing layer];
     //drawing.frame = CGRectMake(2, 2, frame.size.width - 2, frame.size.height - 2);
-    drawing.frame = CGRectMake(2, 2, 26, 26);
+    //drawing.frame = CGRectMake(2, 2, 26, 26);
+    drawing.frame = CGRectMake(0, 0, 21, 21);
     [layer addSublayer:drawing];
     [drawing setNeedsDisplay];
   }
